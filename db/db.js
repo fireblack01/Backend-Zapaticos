30013f486218b022af84c2778e5eaa675b84a5ec
LauraVeraQ <88735474+LauraVeraQ@users.noreply.github.com>
2021-10-17 05:18:20 -0500

    crear base de datos en mongo

--- db/db.js
+++ db/db.js
@@ -18,7 +18,7 @@ const conectarBD = (callback) => {
       console.error('Error conectando a la base de datos');
       return 'error';
     }
-    baseDeDatos = db.db('concesionario');
+    baseDeDatos = db.db('Zapaticos');
     console.log('baseDeDatos exitosa');
     return callback();
   });
